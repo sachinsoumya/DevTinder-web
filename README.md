f50f51355db92525f9f70905d7c548f0a78a2e9e
sachinsoumya <pandasoumyasachin@gmail.com>
2025-04-15 18:10:08 +0530

    Install Tailwind and daisyUI

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript and enable type-aware lint rules. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+# DevTinder
+  - Created Vite + React application
+  - Remove unnecessary codes and code Hello world.
+  - Install tailwind css
+  - Install daisyUi
+  - Add Navbar component to app.jsx file
--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,47 @@ function App() {
 
   return (
     <>
-      <h1>Hello World {count}</h1>
+      <div className="navbar bg-base-300  w-full">
+        <div className="flex-2">
+          <a className="btn btn-ghost text-xl">👨‍💻DevTinder</a>
+        </div>
+        <div className="flex gap-2 mx-2">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      {/* <h1 className="text-red-400 text-2xl">Hello World {count}</h1>
 
-      <button onClick={() => setCount(count + 1)}>Click here</button>
+      <button onClick={() => setCount(count + 1)}>Click here</button> */}
     </>
   );
 }
