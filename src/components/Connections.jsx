a87e086c317e915f2d797ddb8b61f66af09157db
sachinsoumya <pandasoumyasachin@gmail.com>
2025-04-21 17:21:14 +0530

    feat: Connection page

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -26,13 +26,40 @@ const Connections = () => {
     getConnections();
   }, []);
 
-  if (connections) return;
+  if (!connections) return;
 
   if (connections.length === 0) return <h1>No connections Found</h1>;
 
   return (
     <div>
-      <h2>My Connections</h2>
+      <h2 className="text-white text-center font-semibold text-2xl">My Connections</h2>
+      <div className="flex justify-center items-center">
+        <div>
+          {connections.map((item)=> (<div className="card w-96 bg-base-300 card-sm shadow-sm my-2">
+             
+              <div className="card-body ">
+                <div className="flex ">
+                  {" "}
+                  <div className="avatar">
+                    <div className="w-12 rounded-full">
+                      <img src={item.photoUrl} />
+                    </div>
+                  </div>
+                  <div className="mx-2">
+                    <h2 className="card-title">{item.firstName}</h2>
+                    <div>{item.age} , {item.gender}</div>
+                  </div>
+                </div>
+
+                <p>{item.about}</p>
+                <div className="justify-end card-actions">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+           
+          </div>))}
+        </div>
+      </div>
     </div>
   );
 };
